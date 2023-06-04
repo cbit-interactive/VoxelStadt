2d8c9bf6fbd0cd65fe829abf5bfc21f83192f6ae
IIAB <elisover7@gmail.com>
2023-06-04 15:08:47 -0700

    More edits. Long time though

---
+++ src/main/java/org/iiab/lukas/EngineManager.java
@@ -0,0 +1,13 @@
+package org.iiab.lukas;
+
+public class EngineManager {
+
+    public static final long NANOSECOND = 1000000000L; // TIME IS COMING HELL YEAH
+    public static final long FRAMERATE = 1000; // FPS
+
+    private static int fps;
+    private static float frametime = 1.0f / FRAMERATE;
+
+    private boolean isRunning;
+
+}
---
+++ src/main/java/org/iiab/voxelstadt/Launcher.java
@@ -0,0 +1,9 @@
+package org.iiab.voxelstadt;
+
+import org.lwjgl.Version;
+
+public class Launcher {
+    public static void Main(String[] args){
+        System.out.println(Version.getVersion());
+    }
+}
