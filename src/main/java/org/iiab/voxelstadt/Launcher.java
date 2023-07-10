cde250abcd53c53cc41b40e2915c2eedb008184e
IIAB <elisover7@gmail.com>
2023-07-09 17:03:10 -0700

    Deleted a bunch of stuff and preparations to actually start making this thing!

--- src/main/java/org/iiab/voxelstadt/Launcher.java
+++ src/main/java/org/iiab/voxelstadt/Launcher.java
@@ -1,12 +1,7 @@
 package org.iiab.voxelstadt;
 
-import net.iiab.lukas.WindowManager;
-
 public class Launcher {
     public static void main(String[] args){
-        WindowManager windowManager = new WindowManager();
-        windowManager.init();
-        windowManager.run();
-
+        // Hello World!
     }
 }
